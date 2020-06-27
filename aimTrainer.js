0f821593e984d3ae48a3eb403f63f0694f4438b2
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-06-26 21:45:04 -0500

    messing with aim trainer

--- aimTrainer.js
+++ aimTrainer.js
@@ -1,1 +1,10 @@
-
+function changeColor() {
+  document.getElementById("map").addEventListener("click", function () {
+    console.log("hey boa");
+    // if ((document.getElementById("map").style = "background-color: red;")) {
+    //   document.getElementById("map").style = "background-color: blue;";
+    // } else {
+    //   document.getElementById("map").style = "background-color: red;";
+    // }
+  });
+}
