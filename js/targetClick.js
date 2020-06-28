a54a352dc284fecbf925a54c37d5b11b1ac52ead
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-06-27 20:38:56 -0500

    added a timer, and made a directory for all js

--- js/targetClick.js
+++ js/targetClick.js
@@ -1,10 +1,11 @@
 function rand() {
-  return Math.round(Math.random() * 20) - 10;
+  return Math.round(Math.random() * 1000);
 }
 
 let target = document.getElementById("target");
 target.addEventListener("click", function () {
-  const top = 500 + rand();
-  const left = 500 + rand();
+  console.log(rand());
+  const top = rand();
+  const left = rand();
   target.style = `top: ${top}%; left: ${left}; transform: translate(-${top}%, -${left}%);`;
 });
---
+++ js/timer.js
@@ -0,0 +1,5 @@
+let time = 0;
+window.setInterval(function () {
+  time += 1;
+  document.getElementById("time").innerHTML = time;
+}, 1000);
