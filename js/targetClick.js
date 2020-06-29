defdf504ec427fb13527fd15e0fbfe51a9ea9ebb
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-06-28 22:10:46 -0500

    event.stoppropagation keeps div underneath from being clicked. target now has a chance to appear in every quadrant of the map

--- js/targetClick.js
+++ js/targetClick.js
@@ -4,10 +4,22 @@ function rand() {
 
 let hits = 0;
 let target = document.getElementById("target");
-target.addEventListener("click", function () {
+target.addEventListener("click", function (e) {
+  const quadrantArray = [1, 2, 3, 4];
+  const randomQuadrant =
+    quadrantArray[Math.floor(Math.random() * quadrantArray.length)];
   hits += 1;
   document.getElementById("hits").innerHTML = hits;
   const top = rand();
   const left = rand();
-  target.style = `top: ${top}%; left: ${left}; transform: translate(-${top}%, -${left}%);`;
+  if (randomQuadrant === 1) {
+    target.style = `top: ${top}%; left: ${left}; transform: translate(-${top}%, -${left}%);`;
+  } else if (randomQuadrant === 2) {
+    target.style = `top: -${top}%; left: -${left}; transform: translate(-${top}%, ${left}%);`;
+  } else if (randomQuadrant === 3) {
+    target.style = `top: -${top}%; left: -${left}; transform: translate(${top}%, ${left}%);`;
+  } else if (randomQuadrant === 4) {
+    target.style = `top: -${top}%; left: -${left}; transform: translate(${top}%, -${left}%);`;
+  }
+  e.stopPropagation();
 });
